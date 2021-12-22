969b99877f5200e5449f81c682c2f609ff8bd43a
Pavel Karelin <hkarel@yandex.ru>
2021-12-22 18:44:57 +0300

    Обновлены субмодули

--- src/usbrelay_demo/usb_relay_demo.cpp
+++ src/usbrelay_demo/usb_relay_demo.cpp
@@ -134,7 +134,7 @@ int main(int argc, char *argv[])
         mw.saveGeometry();
         mw.deinit();
 
-        config::state().save();
+        config::state().saveFile();
     }
     catch (YAML::ParserException& e)
     {
